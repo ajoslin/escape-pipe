71ad2e94b1f2255a8fa60e948f26ccdb060eefc8
Andrew Joslin <andrewtjoslin@gmail.com>
2016-03-16 15:44:48 -0700

    Rename escapePipe to orPipe internally

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var toArray = require('to-array')
 module.exports = function orPipe () {
   var fns = toArray(arguments)
 
-  return function runEscapePipe () {
+  return function runOrPipe () {
     var result
     for (var i = 0; i < fns.length; i++) {
       result = fns[i].apply(null, arguments)
